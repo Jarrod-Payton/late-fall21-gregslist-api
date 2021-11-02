30bb7b8108884079f0bedbe074885c16a69ba576
Lucas Payton <lucasjpayton101@gmail.com>
2021-11-02 15:27:13 -0600

    Stuff

--- client/app/env.js
+++ client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'mark-codeworks.us.auth0.com'
-export const audience = 'https://MarkDev.com'
-export const clientId = 'Qx0sMBfDbr9hZ916wAVJ6Y4ywBMBV9R2'
+export const domain = 'lucas-codeworks.us.auth0.com'
+export const audience = 'https://GregsList.com'
+export const clientId = 'oSkktRDu9X4acAgJe2G1SrNMFnaeyy3N'
